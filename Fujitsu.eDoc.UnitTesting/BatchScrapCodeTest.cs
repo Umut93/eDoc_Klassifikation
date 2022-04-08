87d770ba160c04195e718799b20ae2fa80faf288
Umut93 <umutkayatuz@live.dk>
2022-04-08 12:48:33 +0200

    Add files via upload

---
+++ Fujitsu.eDoc.UnitTesting/BatchScrapCodeTest.cs
@@ -0,0 +1,91 @@
+﻿using Fujitsu.eDoc.ScrapCode;
+using Fujitsu.eDoc.ScrapCode.Ulitity;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace Fujitsu.eDoc.UnitTesting
+{
+    [TestClass]
+    public class BatchScrapCodeTest
+    {
+        [TestMethod, TestCategory("KLE")]
+        public void CallEmnePlanAPI_Result_OK()
+        {
+            //Act
+            MethodInfo dynMethod = typeof(WebRequester).GetMethod("GetEmnePlaner", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)dynMethod.Invoke(null, null);
+            //Assertion
+            Assert.IsNotNull(result);
+        }
+
+
+        [TestMethod, TestCategory("KLE")]
+        public void CallFacetAPI_Result_OK()
+        {
+            //Act
+            MethodInfo dynMethod = typeof(WebRequester).GetMethod("GetFacets", BindingFlags.Static | BindingFlags.Public);
+            string result = (string)dynMethod.Invoke(null, null);
+            //Assertion
+            Assert.IsNotNull(result);
+        }
+
+
+
+        [TestMethod, TestCategory("KLE")]
+        public void UpdateEmnePlan_Result_OK()
+        {
+            var isUpdated = false;
+
+            using (ShimsContext.Create())
+            {
+                var emnePlan = new KLEEmneplanHovedgruppeGruppeEmne();
+                emnePlan.BevaringJaevnfoerArkivloven = "B";
+                emnePlan.SletningJaevnfoerPersondataloven = "5";
+                emnePlan.EmneNr = "01.01.01";
+
+                Fujitsu.eDoc.ScrapCode.Fakes.ShimDbConnection.Constructor = (@this) => { };
+                Dapper.Fakes.ShimSqlMapper.ExecuteIDbConnectionStringObjectIDbTransactionNullableOfInt32NullableOfCommandType = (a, b, c, d, e, f) =>
+                {
+                    isUpdated = true;
+                    return 1;
+                };
+
+                //Act
+                MethodInfo dynMethod = typeof(NovClassCodeRepo).GetMethod("UpdateEmnePlan", BindingFlags.Instance | BindingFlags.Public);
+                INovClassCodeRepo novRepo = new NovClassCodeRepo();
+                dynMethod.Invoke(novRepo, new object[] { emnePlan });
+
+                //Assertion
+                Assert.IsTrue(isUpdated);
+            }
+        }
+
+        [TestMethod, TestCategory("KLE")]
+        public void UpdateFacet_Result_OK()
+        {
+            var isUpdated = false;
+
+            using (ShimsContext.Create())
+            {
+                var facet = new KLEHandlingsfacetterHandlingsfacetKategoriHandlingsfacet();
+                facet.BevaringJaevnfoerArkivloven = "K10";
+
+                Fujitsu.eDoc.ScrapCode.Fakes.ShimDbConnection.Constructor = (@this) => { };
+                Dapper.Fakes.ShimSqlMapper.ExecuteIDbConnectionStringObjectIDbTransactionNullableOfInt32NullableOfCommandType = (a, b, c, d, e, f) =>
+                {
+                    isUpdated = true;
+                    return 1;
+                };
+
+                //Act
+                MethodInfo dynMethod = typeof(NovClassCodeRepo).GetMethod("UpdateFacet", BindingFlags.Instance | BindingFlags.Public);
+                INovClassCodeRepo novRepo = new NovClassCodeRepo();
+                dynMethod.Invoke(novRepo, new object[] { facet });
+
+                //Assertion
+                Assert.IsTrue(isUpdated);
+            }
+        }
+    }
+}
---
+++ Fujitsu.eDoc.UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fujitsu.eDoc.UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fujitsu.eDoc.UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb307751-7c73-4fe9-8f1b-bee70c298dc7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
